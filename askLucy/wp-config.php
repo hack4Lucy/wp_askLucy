fe48ca91b52423b21a4165231351df4ff33a0941
Hailemichael M. Worku <hack4Lucy@gmail.com>
2016-07-10 00:46:49 +0200

    Update wp-config.php

--- askLucy/wp-config.php
+++ askLucy/wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'wp_askLucy');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Bob1@Saron2');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
